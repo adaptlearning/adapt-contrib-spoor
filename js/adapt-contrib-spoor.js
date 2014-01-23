baf5111af6de2348d5c3527a7e6c697aa63ff980
Kev Adsett <kev.adsett@gmail.com>
2014-01-23 16:53:55 +0000

    Initial spoor fixes

--- js/adapt-contrib-spoor.js
+++ js/adapt-contrib-spoor.js
@@ -6,68 +6,43 @@
 define(function(require) {
 
   var Adapt = require('coreJS/adapt');
-  var sAPI = require('extensions/adapt-contrib-spoor/js/SCORM_API_wrapper');
-  var sWrapper = require('extensions/adapt-contrib-spoor/js/scormWrapper');
-  var sLog = require('extensions/adapt-contrib-spoor/js/logger');
+  var scormAPI = require('extensions/adapt-contrib-spoor/js/SCORM_API_wrapper');
+  var scormWrapper = require('extensions/adapt-contrib-spoor/js/scormWrapper').getInstance();
+  var scormLog = require('extensions/adapt-contrib-spoor/js/logger');
   
   var Spoor = Backbone.Model.extend({
 
     defaults: {
       initialised: false,
       SCOFinishCalled: false,
-      dataIsFromLMS: true,
       testingMode: false
     },
 
-    url: "spoor/scormData.json",
-
     initialize: function() {
       //_.bindAll(this);
-      this.fetch();
-      this.SCOStart();
-    },
-    
-    init: function() {
-      if (Adapt.course !== undefined && Adapt.course.get('dataReady')) {
+        this.data = Adapt.config.get('_spoor');
+        console.log(scormWrapper);
+        this.SCOStart() 
+        $(window).unload(_.bind(this.SCOFinish, this));
         this.onDataReady();
-      } else {
-        Adapt.on('app:dataReady', this.onDataReady, this);
-      }
-
-      
-      //Adapt.on('itemComplete', this.onItemComplete, this);
     },
 
     checkCompletionCriteria: function() {
-      var needModsComplete = this.get('tracking').requireAllModulesCompleted;
-      var needQuizPassed = this.get('tracking').requireQuizPassed;
+      var requirementArray = [this.data._tracking._requireCourseCompleted, this.data._tracking._requireAssessmentPassed];
+      var actualArray = [Adapt.course.get('_isComplete'), Adapt.course.get('_isAssessmentPassed')];
+      
+      var criteriaMet = true;
 
-      needModsComplete = this.parseBool(needModsComplete);
-      needQuizPassed = this.parseBool(needQuizPassed);
-
-      var quizPassed = !!Adapt.course.get('quizPassed');
-      var modulesCompleted = !!Adapt.course.get('_isComplete');
-      var trackingCriteriaMet = false;
-
-      if (needModsComplete) {
-        if (modulesCompleted) {
-          if (needQuizPassed) {
-            if (quizPassed) {
-              trackingCriteriaMet = true;
-            }
-          } else {
-            trackingCriteriaMet = true;
+      _.each(requirementArray, function(requirement, index) {
+        if(requirement) {
+          if(!actualArray[index]) {
+            criteriaMet = false;
           }
         }
-      } else {
-        if (needQuizPassed) {
-          if (quizPassed) {
-            trackingCriteriaMet = true;
-          }
-        }
-      }
-      if (trackingCriteriaMet) {
-        this.setLessonStatus(this.get('reporting').onTrackingCriteriaMet);
+      }, this);
+
+      if(criteriaMet) {
+        this.setLessonStatus(this.data._reporting.onTrackingCriteriaMet);
       }
     },
 
@@ -89,28 +64,28 @@ define(function(require) {
     },
 
     createCompletionString: function() {
-      var blockCompletionArray = this.get('blockCompletionArray');
+      var _blockCompletionArray = this.get('_blockCompletionArray');
 
       _.each(Adapt.blocks.models, function(blockModel) {
         if (blockModel.get('_isComplete')) {
-          blockCompletionArray[blockModel.get('trackingId')] =  1;
+          _blockCompletionArray[blockModel.get('_trackingId')] =  1;
         }
       }, this);
 
-      this.set('blockCompletionArray', blockCompletionArray);
-      return this.convertCompletionArrayToString(blockCompletionArray);
+      this.set('_blockCompletionArray', _blockCompletionArray);
+      return this.convertCompletionArrayToString(_blockCompletionArray);
     },
 
     createCompletionObject: function() {
       var completionObject = {spoor:{}};
       completionObject.spoor.completion = this.createCompletionString();
-      completionObject.spoor.quizPassed = Adapt.course.get('quizPassed') || false;
+      completionObject.spoor._isAssessmentPassed = Adapt.course.get('_isAssessmentPassed') || false;
       this.set('completionObject', completionObject);
       return completionObject;
     },
 
     getDataIsAlreadyReported: function(completionObject) {
-      return JSON.stringify(this.get('suspendData')) === JSON.stringify(completionObject);
+      return JSON.stringify(this.get('_suspendData')) === JSON.stringify(completionObject);
     },
 
     markBlockAsComplete: function(parameters) {
@@ -120,7 +95,7 @@ define(function(require) {
         return;
       }
 
-      _.each(block.getChildren(), function(child) {
+      block.getChildren().each(function(child) {
         child.set('_isComplete', true);
       }, this);
     },
@@ -137,83 +112,81 @@ define(function(require) {
       this.setSessionData();
       this.setupBlockCompletionData();
       this.repopulateCompletionData();
-      Adapt.blocks.on('change:_isComplete', this.onItemComplete, this);
-      Adapt.trigger("spoorReady");
+      this.listenToCompletionEvents();
+      console.log(Adapt.blocks);
+      // may no longer be needed - DH
+//      Adapt.trigger("spoorReady");
     },
-
-    onItemComplete: function(item) {
-      console.log('onItemComplete');
-      if(!this.get('dataIsFromLMS')) {
-        switch(item.get('_type')) {
-          case "course":
-            this.onCriterionMet("complete");
-            break;
-          case "block":
-            this.set('lastCompletedBlock', item);
-            this.sendCompletionString();
-            break;
-        }
-      }
+    
+    listenToCompletionEvents: function() {
+      Adapt.blocks.on('change:_isComplete', this.onBlockComplete, this);
+      Adapt.course.on('change:_isComplete', this.onCourseComplete, this);
+      Adapt.on('assessment:complete', this.onAssessmentComplete, this);
     },
-
-    onQuizComplete: function(isAssessment, quizPassed) {
-      console.info("Spoor::onQuizComplete: assessment: " + isAssessment + ", quizPassed: " + quizPassed);
-      if (isAssessment) {
-        if (quizPassed) {
-          this.onCriterionMet("quizPassed");
-        } else {
-          var onQuizFailure = this.get('reporting').onQuizFailure;
-          if (onQuizFailure !== "" && onQuizFailure !== "incomplete") {
-            this.setLessonStatus(onQuizFailure);
-          }
+      
+    onCourseComplete: function() {
+      this.onCriterionMet("_isComplete");
+    },
+    
+    onBlockComplete: function(block) {
+      this.set('lastCompletedBlock', block);
+      this.sendCompletionString();
+    },
+    
+    onAssessmentComplete: function(event) {
+      var _isAssessmentPassed = event.isPass;
+      console.info("Spoor::onAssessmentComplete: assessment: " + isAssessment + ", _isAssessmentPassed: " + _isAssessmentPassed);
+      if (_isAssessmentPassed) {
+        this.onCriterionMet("_isAssessmentPassed");
+      } else {
+        var onAssessmentFailure = this.data._reporting.onAssessmentFailure;
+        if (onAssessmentFailure !== "" && onAssessmentFailure !== "incomplete") {
+          this.setLessonStatus(onAssessmentFailure);
         }
       }
     },
 
     repopulateCompletionData: function() {
-      var suspendData = this.get('suspendData');
+      var suspendData = this.get('_suspendData');
 
       if (suspendData.spoor.completion !== "") {
         this.restoreProgress(suspendData);
       }
-
-      this.set('dataIsFromLMS', false);
     },
 
     restoreProgress: function(suspendData) {
-      if (this.get('scormWrapper').getStatus() === "courseComplete") {
+      if (scormWrapper.getStatus() === "courseComplete") {
         Adapt.course.setOnChildren('_isComplete', true);
       } else {
-        _.each(this.get('blockCompletionArray'), function(blockCompletion, blockTrackingId) {
+        _.each(this.get('_blockCompletionArray'), function(blockCompletion, blockTrackingId) {
           if (blockCompletion === 1) {
-            this.markBlockAsComplete({block: Adapt.blocks.findWhere({trackingId: blockTrackingId}), includeChildren: true});
+            this.markBlockAsComplete({block: Adapt.blocks.findWhere({_trackingId: blockTrackingId}), includeChildren: true});
           }
         }, this);
       }
-      Adapt.course.set('quizPassed', suspendData.spoor.quizPassed);
-      this.set("suspendData", suspendData);
+      Adapt.course.set('_isAssessmentPassed', suspendData.spoor._isAssessmentPassed);
+      this.set('_suspendData', suspendData);
       this.sendCompletionString();
     },
 
     SCOFinish:function() {
-      if (!this.get('SCOFinishCalled')) {
+      if (!this.get('_SCOFinishCalled')) {
         this.set('SCOFinishCalled', true);
-        this.get('scormWrapper').finish();
+        scormWrapper.finish();
       }
     },
 
     SCOStart: function() {
-      this.set('scormWrapper', this.get('testingMode') ? this.get('testingLMS') : ScormWrapper.getInstance());
-      var sw = this.get('scormWrapper');
-      sw.setVersion("1.2");
+      // this.set('scormWrapper', this.data._testingMode') ? this.get('testingLMS : ScormWrapper.getInstance());
+      var sw = scormWrapper;
       if (sw.initialize()) {
+        sw.setVersion("1.2");
         this.set('initialised', true);
         var lessonStatus = sw.getStatus().toLowerCase();
 
         if (lessonStatus == "not attempted" || lessonStatus == "unknown") {
           sw.setIncomplete();
         }
-        this.init();
       }
     },
 
@@ -221,30 +194,30 @@ define(function(require) {
       console.log('sendCompletionString');
       var suspendData = this.createCompletionObject();
       if (!this.getDataIsAlreadyReported(suspendData)) {
-        if (this.get('lastCompletedBlock') !== undefined) {
-          console.info("Spoor: block " + this.get('lastCompletedBlock').get('id') + " is complete.");
+        if (this.get('_lastCompletedBlock') !== undefined) {
+          console.info("Spoor: block " + this.get('_lastCompletedBlock').get('id') + " is complete.");
         }
         this.set({
-          suspendData: suspendData, 
-          blockCompletionArray: this.convertCompletionStringToArray(suspendData.spoor.completion)
+          _suspendData: suspendData, 
+          _blockCompletionArray: this.convertCompletionStringToArray(suspendData.spoor.completion)
         });
-        this.get('scormWrapper').setSuspendData(JSON.stringify(suspendData));
+        scormWrapper.setSuspendData(JSON.stringify(suspendData));
       }
     },
 
     setLessonStatus:function(status){
       switch (status){
         case "incomplete":
-          this.get('scormWrapper').setIncomplete();
+          scormWrapper.setIncomplete();
           break;
         case "completed":
-          this.get('scormWrapper').setCompleted();
+          scormWrapper.setCompleted();
           break;
         case "passed":
-          this.get('scormWrapper').setPassed();
+          scormWrapper.setPassed();
           break;
         case "failed":
-          this.get('scormWrapper').setFailed();
+          scormWrapper.setFailed();
           break;
         default:
           console.warn("cmi.core.lesson_status of " + status + " is not supported.");
@@ -253,21 +226,21 @@ define(function(require) {
     },
 
     setScore: function(score){
-      this.get('scormWrapper').setScore(score,0,100);
+      scormWrapper.setScore(score,0,100);
     },
 
     setSessionData: function() {
-      var suspendData = this.get('scormWrapper').getSuspendData();
+      var suspendData = scormWrapper.getSuspendData();
 
       if (suspendData === "" || suspendData === " " || suspendData === undefined) {
-        this.set('suspendData', {
+        this.set('_suspendData', {
           spoor: {
             completion: "",
-            quizPassed: false
+            _isAssessmentPassed: false
           }
         });
       } else {
-        this.set('suspendData', JSON.parse(suspendData));
+        this.set('_suspendData', JSON.parse(suspendData));
       }
 
       this.set({
@@ -281,27 +254,27 @@ define(function(require) {
     },
 
     setupBlockCompletionData: function() {
-      if (Adapt.course.get('latestTrackingId') === undefined) {
+      if (Adapt.course.get('_latestTrackingId') === undefined) {
         var message = "This course is missing a latestTrackingID.\n\nPlease run the grunt process prior to deploying this module on LMS.\n\nScorm tracking will not work correctly until this is done.";
         alert(message);
         console.error(message);
       }
-      var blockCompletionArray = new Array(Adapt.course.get('latestTrackingId') + 1);
-      for (var i = 0; i < blockCompletionArray.length; i++) {
-        blockCompletionArray[i] = -1;
+      var _blockCompletionArray = new Array(Adapt.course.get('_latestTrackingId') + 1);
+      for (var i = 0; i < _blockCompletionArray.length; i++) {
+        _blockCompletionArray[i] = -1;
       }
 
       _.each(Adapt.blocks.models, function(model, index) {
-        var trackingId = model.get('trackingId');
-        var recordedCompletion = parseInt(this.get('suspendData').spoor.completion[trackingId], 10) || 0;
-        if (trackingId === undefined) {
+        var _trackingId = model.get('_trackingId');
+        var recordedCompletion = parseInt(this.get('_suspendData').spoor.completion[_trackingId], 10) || 0;
+        if (_trackingId === undefined) {
           var message = "Block '" + model.get('id') + "' doesn't have a tracking ID assigned.\n\nPlease run the grunt process prior to deploying this module on LMS.\n\nScorm tracking will not work correctly until this is done.";
           alert(message);
           console.error(message);
         }
-        blockCompletionArray[trackingId] = recordedCompletion;
+        _blockCompletionArray[_trackingId] = recordedCompletion;
       }, this);
-      this.set('blockCompletionArray', blockCompletionArray);
+      this.set('_blockCompletionArray', _blockCompletionArray);
     },
 
     parseBool: function(str) {
@@ -317,7 +290,7 @@ define(function(require) {
     }
 
   });
-
-  return new Spoor();
-
+  Adapt.on('app:dataReady', function() {
+    new Spoor();
+  })
 });
