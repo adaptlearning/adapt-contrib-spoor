331e9d5addc5583100bfd3450844e8e842e8c1ae
Kev Adsett <kev.adsett@gmail.com>
2014-01-23 17:02:25 +0000

    Add offline player

---
+++ required/offline_API_wrapper.js
@@ -0,0 +1,10 @@
+var API = {
+	LMSInitialize: function() {},
+	LMSFinish: function() {},
+	LMSGetValue: function() {},
+	LMSSetValue: function() {},
+	LMSCommit: function() {},
+	LMSGetLastError: function() {},
+	LMSGetErrorString: function() {},
+	LMSGetDiagnostic: function() {}
+}
