376735acbda900728380d12a514d087bf7871a2c
moloko <mleathes+github@gmail.com>
2015-06-08 20:11:11 +0100

    trim trailing spaces

--- js/adapt-contrib-spoor.js
+++ js/adapt-contrib-spoor.js
@@ -41,14 +41,14 @@ define([
 
 		startSCO: function() {
 			/**
-			* force use of SCORM 1.2 - as some LMSes (SABA, for instance) present both APIs to the SCO and, if given the choice, 
+			* force use of SCORM 1.2 - as some LMSes (SABA, for instance) present both APIs to the SCO and, if given the choice,
 			* the pipwerks code will automatically select the SCORM 2004 API - which can lead to unexpected behaviour.
 			* this does obviously mean you'll have to manually change (or just remove) this next line if you want SCORM 2004 output
 			*/
 			//TODO allow version to be set via config.json
 			scorm.setVersion("1.2");
 			scorm.initialize()
-		},	
+		},
 
 		setupEventListeners: function() {
 			this._onWindowUnload = _.bind(this.onWindowUnload, this);
@@ -62,7 +62,7 @@ define([
 			this.removeEventListeners();
 		},
 
-		endSCO: function() {			
+		endSCO: function() {
 			scorm.finish();
 		},
 
