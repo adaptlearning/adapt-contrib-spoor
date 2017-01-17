5b1e79bcc219a859c77ed8cc1d4a59a4807725e6
Jack Ward <jackward@live.co.uk>
2017-01-17 12:42:42 +0000

    Wrapped SCORM finish in finishCalled check
    Another component/extension in a course may have already called finish against SCORM (close button for example).

--- js/adapt-contrib-spoor.js
+++ js/adapt-contrib-spoor.js
@@ -126,7 +126,9 @@ define([
         onWindowUnload: function() {
             $(window).off('beforeunload unload', this._onWindowUnload);
 
-            scorm.finish();
+            if (!scorm.finishCalled){
+                scorm.finish();
+            }
         }
         
     }, Backbone.Events);
