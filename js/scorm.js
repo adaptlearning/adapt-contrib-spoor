32d7b755616cc1770512e11c2fa989f3dd8251d2
moloko <mleathes+github@gmail.com>
2018-08-14 17:59:52 +0100

    tabs > spaces, braces in for loops, shorthand array creation, semi-colons

--- js/scorm.js
+++ js/scorm.js
@@ -1,11 +1,11 @@
 define([
-	'./scorm/API',
- 	'./scorm/wrapper',
-	'./scorm/logger'
+    './scorm/API',
+    './scorm/wrapper',
+    './scorm/logger'
 ], function(API, wrapper, logger) {
 
-	//Load and prepare SCORM API
+    //Load and prepare SCORM API
 
-	return wrapper.getInstance();
+    return wrapper.getInstance();
 
 });
