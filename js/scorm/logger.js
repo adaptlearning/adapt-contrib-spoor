68eb444bac3926b69da56cb98061b142d3664fe6
Oliver Foster <oliver.foster@kineo.com>
2020-08-03 11:58:53 +0100

    issue/2805: Allowed component trackingIds and reworked to ES6 (#197)

--- js/scorm/logger.js
+++ js/scorm/logger.js
@@ -1,68 +1,74 @@
-Logger = function() {
-  this.logArr = [];
-  this.registeredViews = [];
-};
+define(function() {
 
-// static
-Logger.instance = null;
-Logger.LOG_TYPE_INFO = 0;
-Logger.LOG_TYPE_WARN = 1;
-Logger.LOG_TYPE_ERROR = 2;
-Logger.LOG_TYPE_DEBUG = 3;
+  class Logger {
 
-Logger.getInstance = function() {
-  if (Logger.instance == null)
-    Logger.instance = new Logger();
-  return Logger.instance;
-};
-
-Logger.prototype.getEntries = function() {
-  return this.logArr;
-};
-
-Logger.prototype.getLastEntry = function() {
-  return this.logArr[this.logArr.length - 1];
-};
-
-Logger.prototype.info = function(str) {
-  this.logArr[this.logArr.length] = {str:str, type:Logger.LOG_TYPE_INFO, time:Date.now()};
-  this.updateViews();
-};
-
-Logger.prototype.warn = function(str) {
-  this.logArr[this.logArr.length] = {str:str, type:Logger.LOG_TYPE_WARN, time:Date.now()};
-  this.updateViews();
-};
-
-Logger.prototype.error = function(str) {
-  this.logArr[this.logArr.length] = {str:str, type:Logger.LOG_TYPE_ERROR, time:Date.now()};
-  this.updateViews();
-};
-
-Logger.prototype.debug = function(str) {
-  this.logArr[this.logArr.length] = {str:str, type:Logger.LOG_TYPE_DEBUG, time:Date.now()};
-  this.updateViews();
-};
-
-//register a view
-Logger.prototype.registerView = function(_view) {
-  this.registeredViews[this.registeredViews.length] = _view;
-};
-
-//unregister a view
-Logger.prototype.unregisterView = function(_view) {
-  for (var i = 0; i < this.registeredViews.length; i++) {
-    if (this.registeredViews[i] == _view) {
-      this.registeredViews.splice(i, 1);
-      i--;
+    constructor() {
+      this.logArr = [];
+      this.registeredViews = [];
     }
-  }
-};
 
-// update all views
-Logger.prototype.updateViews = function() {
-  for (var i = 0; i < this.registeredViews.length; i++) {
-    if (this.registeredViews[i])
+    static getInstance() {
+      if (Logger.instance === null) {
+        Logger.instance = new Logger();
+      }
+      return Logger.instance;
+    }
+
+    getEntries() {
+      return this.logArr;
+    }
+
+    getLastEntry() {
+      return this.logArr[this.logArr.length - 1];
+    }
+
+    info(str) {
+      this.logArr[this.logArr.length] = { str: str, type: Logger.LOG_TYPE_INFO, time: Date.now() };
+      this.updateViews();
+    }
+
+    warn(str) {
+      this.logArr[this.logArr.length] = { str: str, type: Logger.LOG_TYPE_WARN, time: Date.now() };
+      this.updateViews();
+    }
+
+    error(str) {
+      this.logArr[this.logArr.length] = { str: str, type: Logger.LOG_TYPE_ERROR, time: Date.now() };
+      this.updateViews();
+    }
+
+    debug(str) {
+      this.logArr[this.logArr.length] = { str: str, type: Logger.LOG_TYPE_DEBUG, time: Date.now() };
+      this.updateViews();
+    }
+
+    registerView(_view) {
+      this.registeredViews[this.registeredViews.length] = _view;
+    }
+
+    unregisterView(_view) {
+      for (let i = 0, l = this.registeredViews.length; i < l; i++) {
+        if (this.registeredViews[i] !== _view) continue;
+        this.registeredViews.splice(i, 1);
+        i--;
+      }
+    }
+
+    updateViews() {
+      for (let i = 0, l = this.registeredViews.length; i < l; i++) {
+        if (!this.registeredViews[i]) continue;
         this.registeredViews[i].update(this);
+      }
+    }
+
   }
-};
+
+  Logger.instance = null;
+  Logger.LOG_TYPE_INFO = 0;
+  Logger.LOG_TYPE_WARN = 1;
+  Logger.LOG_TYPE_ERROR = 2;
+  Logger.LOG_TYPE_DEBUG = 3;
+
+  return Logger;
+
+});
