e658c27a9e78c853b4a10f27907a93124908a511
danielghost <daniel.ghost@kineo.com>
2022-11-18 10:03:15 +0000

    Added ability to compile as SCORM 1.2 or SCORM 2004 (curently limited to SCROM 2004 4th Edition) - fixes #249.

---
+++ scripts/postcopy.js
@@ -0,0 +1,24 @@
+
+module.exports = async function(fs, path, log, options, done) {
+  const buildPath = options.outputdir;
+  const coursePath = `${buildPath}course`;
+  const data = await fs.promises.readFile(`${coursePath}/config.json`).catch(error => {
+    log(error);
+  });
+  if (!data) return done();
+  const config = JSON.parse(data.toString());
+  const scormVersion = config?._spoor?._advancedSettings?._scormVersion ?? '1.2';
+  const scormPath = `${options.plugindir}/scorm/${scormVersion}`;
+  const files = await fs.promises.readdir(`${scormPath}`).catch(error => {
+    log(error);
+  });
+  if (!files) return done();
+  //log(`Adding SCORM ${scormVersion} files`);
+  const promises = files.map(file => {
+    return fs.promises.copyFile(`${scormPath}/${file}`, `${buildPath}/${file}`).catch(error => {
+      log(error);
+    });
+  });
+  await Promise.all(promises);
+  done();
+};
