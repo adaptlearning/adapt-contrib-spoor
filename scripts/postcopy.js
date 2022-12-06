c1f429737ce8aaf39f276e970b9530450d97daec
Oliver Foster <oliver.foster@kineo.com>
2022-12-06 15:19:51 +0000

    Switch from .catch to try catch on promises

--- scripts/postcopy.js
+++ scripts/postcopy.js
@@ -1,23 +1,18 @@
 
 module.exports = async function(fs, path, log, options, done) {
-  const buildPath = options.outputdir;
-  const coursePath = `${buildPath}course`;
-  const data = await fs.promises.readFile(`${coursePath}/config.json`).catch(error => {
-    log(error);
-  });
-  if (!data) return done();
-  const config = JSON.parse(data.toString());
-  const scormVersion = config?._spoor?._advancedSettings?._scormVersion ?? '1.2';
-  const scormPath = `${options.plugindir}/scorm/${scormVersion}`;
-  const files = await fs.promises.readdir(`${scormPath}`).catch(error => {
-    log(error);
-  });
-  if (!files) return done();
-  const promises = files.map(file => {
-    return fs.promises.copyFile(`${scormPath}/${file}`, `${buildPath}/${file}`).catch(error => {
-      log(error);
-    });
-  });
-  await Promise.all(promises);
+  try {
+    const buildPath = options.outputdir;
+    const coursePath = `${buildPath}course`;
+    const data = await fs.promises.readFile(`${coursePath}/config.json`);
+    if (!data) return done();
+    const config = JSON.parse(data.toString());
+    const scormVersion = config?._spoor?._advancedSettings?._scormVersion ?? '1.2';
+    const scormPath = `${options.plugindir}/scorm/${scormVersion}`;
+    const files = await fs.promises.readdir(`${scormPath}`);
+    if (!files) return done();
+    await Promise.all(files.map(file => fs.promises.copyFile(`${scormPath}/${file}`, `${buildPath}/${file}`)));
+  } catch (err) {
+    log(err);
+  }
   done();
 };
