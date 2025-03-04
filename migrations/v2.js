92e9b6a0bba1ea7158bf6581ddc34184e88cd937
joe-allen-89 <85872286+joe-allen-89@users.noreply.github.com>
2025-03-04 00:12:50 +0000

    automated testing added to migration scripts

--- migrations/v2.js
+++ migrations/v2.js
@@ -5,7 +5,9 @@ import {
   mutateContent,
   checkContent,
   updatePlugin,
-  getConfig
+  getConfig,
+  testSuccessWhere,
+  testStopWhere
 } from 'adapt-migrations';
 import _ from 'lodash';
 
@@ -26,92 +28,209 @@ describe('adapt-contrib-spoor - to v2.0.2', async () => {
   const shouldStoreResponsesPath = '_tracking._shouldStoreResponses';
   const scormVersionPath = '_advancedSettings._scormVersion';
   whereFromPlugin('adapt-contrib-spoor - from <v2.0.2', { name: 'adapt-contrib-spoor', version: '<2.0.2' });
+
   whereContent('adapt-contrib-spoor - where _spoor', async () => {
     spoorConfig = getSpoorConfig();
     return spoorConfig;
   });
+
   mutateContent('adapt-contrib-spoor - add _spoor._tracking._shouldStoreResponses', async () => {
     if (!_.has(spoorConfig, shouldStoreResponsesPath)) _.set(spoorConfig, shouldStoreResponsesPath, true);
     return true;
   });
+
+  mutateContent('adapt-contrib-spoor - remove _spoor._advancedSettings._scormVersion', async () => {
+    _.unset(spoorConfig, scormVersionPath);
+    return true;
+  });
+
   checkContent('adapt-contrib-spoor - check _spoor._tracking._shouldStoreResponses added', async () => {
     const isValid = _.has(spoorConfig, shouldStoreResponsesPath);
     if (!isValid) throw new Error(`_spoor.${shouldStoreResponsesPath} not added`);
     return true;
   });
-  mutateContent('adapt-contrib-spoor - remove _spoor._advancedSettings._scormVersion', async () => {
-    _.unset(spoorConfig, scormVersionPath);
-    return true;
-  });
+
   checkContent('adapt-contrib-spoor - check _spoor._advancedSettings._scormVersion removed', async () => {
     const isValid = !_.has(spoorConfig, scormVersionPath);
     if (!isValid) throw new Error(`_spoor.${scormVersionPath} not removed`);
     return true;
   });
   updatePlugin('adapt-contrib-spoor - update to v2.0.2', { name: 'adapt-contrib-spoor', version: '2.0.2', framework: '>=2.0.0' });
+
+  testSuccessWhere('config with empty spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.1' }],
+    content: [
+      { _type: 'config', _spoor: {} }
+    ]
+  });
+
+  testSuccessWhere('config with spoor._tracking/spoor._advancedSettings', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.1' }],
+    content: [
+      { _type: 'config', _spoor: { _tracking: {}, _advancedSettings: {} } }
+    ]
+  });
+
+  testStopWhere('no spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.1' }],
+    content: [
+      { _type: 'config' }
+    ]
+  });
+
+  testStopWhere('spoor incorrect version', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.2' }]
+  });
 });
 
 describe('adapt-contrib-spoor - to v2.0.5', async () => {
   let spoorConfig;
   const suppressErrorsPath = '_advancedSettings._suppressErrors';
   whereFromPlugin('adapt-contrib-spoor - from <v2.0.5', { name: 'adapt-contrib-spoor', version: '<2.0.5' });
+
   whereContent('adapt-contrib-spoor - where missing _spoor._advancedSettings._suppressErrors', async () => {
     spoorConfig = getSpoorConfig();
     if (!spoorConfig) return false;
     return !_.has(spoorConfig, suppressErrorsPath);
   });
+
   mutateContent('adapt-contrib-spoor - add _spoor._advancedSettings._suppressErrors', async () => {
     _.set(spoorConfig, suppressErrorsPath, false);
     return true;
   });
+
   checkContent('adapt-contrib-spoor - check _spoor._advancedSettings._suppressErrors added', async () => {
     const isValid = _.has(spoorConfig, suppressErrorsPath);
     if (!isValid) throw new Error(`_spoor.${suppressErrorsPath} not added`);
     return true;
   });
+
   updatePlugin('adapt-contrib-spoor - update to v2.0.5', { name: 'adapt-contrib-spoor', version: '2.0.5', framework: '>=2.0.0' });
+
+  testSuccessWhere('config with empty spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.4' }],
+    content: [
+      { _type: 'config', _spoor: {} }
+    ]
+  });
+
+  testSuccessWhere('config with spoor._advancedSettings', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.4' }],
+    content: [
+      { _type: 'config', _spoor: { _advancedSettings: {} } }
+    ]
+  });
+
+  testStopWhere('no spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.4' }],
+    content: [
+      { _type: 'config' }
+    ]
+  });
+
+  testStopWhere('spoor incorrect version', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.5' }]
+  });
 });
 
 describe('adapt-contrib-spoor - to v2.0.11', async () => {
   let spoorConfig;
   const commitOnVisibilityChangeHiddenPath = '_advancedSettings._commitOnVisibilityChangeHidden';
   whereFromPlugin('adapt-contrib-spoor - from <v2.0.11', { name: 'adapt-contrib-spoor', version: '<2.0.11' });
+
   whereContent('adapt-contrib-spoor - where missing _spoor._advancedSettings._commitOnVisibilityChangeHidden', async () => {
     spoorConfig = getSpoorConfig();
     if (!spoorConfig) return false;
     return !_.has(spoorConfig, commitOnVisibilityChangeHiddenPath);
   });
+
   mutateContent('adapt-contrib-spoor - add _spoor._advancedSettings._commitOnVisibilityChangeHidden', async () => {
     _.set(spoorConfig, commitOnVisibilityChangeHiddenPath, true);
     return true;
   });
+
   checkContent('adapt-contrib-spoor - check _spoor._advancedSettings._commitOnVisibilityChangeHidden added', async () => {
     const isValid = _.has(spoorConfig, commitOnVisibilityChangeHiddenPath);
     if (!isValid) throw new Error(`_spoor.${commitOnVisibilityChangeHiddenPath} not added`);
     return true;
   });
+
   updatePlugin('adapt-contrib-spoor - update to v2.0.11', { name: 'adapt-contrib-spoor', version: '2.0.11', framework: '>=2.0.0' });
+
+  testSuccessWhere('config with empty spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.10' }],
+    content: [
+      { _type: 'config', _spoor: {} }
+    ]
+  });
+
+  testSuccessWhere('config with spoor._advancedSettings', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.10' }],
+    content: [
+      { _type: 'config', _spoor: { _advancedSettings: {} } }
+    ]
+  });
+
+  testStopWhere('no spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.10' }],
+    content: [
+      { _type: 'config' }
+    ]
+  });
+
+  testStopWhere('spoor incorrect version', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.11' }]
+  });
 });
 
 describe('adapt-contrib-spoor - to v2.0.13', async () => {
   let spoorConfig;
   const resetStatusOnLanguageChangePath = '_reporting._resetStatusOnLanguageChange';
   whereFromPlugin('adapt-contrib-spoor - from <v2.0.13', { name: 'adapt-contrib-spoor', version: '<2.0.13' });
+
   whereContent('adapt-contrib-spoor - where missing _spoor._reporting._resetStatusOnLanguageChange', async () => {
     spoorConfig = getSpoorConfig();
     if (!spoorConfig) return false;
     return !_.has(spoorConfig, resetStatusOnLanguageChangePath);
   });
+
   mutateContent('adapt-contrib-spoor - add _spoor._reporting._resetStatusOnLanguageChange', async () => {
     _.set(spoorConfig, resetStatusOnLanguageChangePath, false);
     return true;
   });
+
   checkContent('adapt-contrib-spoor - check _spoor._reporting._resetStatusOnLanguageChange added', async () => {
     const isValid = _.has(spoorConfig, resetStatusOnLanguageChangePath);
     if (!isValid) throw new Error(`_spoor.${resetStatusOnLanguageChangePath} not added`);
     return true;
   });
+
   updatePlugin('adapt-contrib-spoor - update to v2.0.13', { name: 'adapt-contrib-spoor', version: '2.0.13', framework: '>=2.0.0' });
+
+  testSuccessWhere('config with empty spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.12' }],
+    content: [
+      { _type: 'config', _spoor: {} }
+    ]
+  });
+
+  testSuccessWhere('config with spoor._reporting', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.12' }],
+    content: [
+      { _type: 'config', _spoor: { _reporting: {} } }
+    ]
+  });
+
+  testStopWhere('no spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.12' }],
+    content: [
+      { _type: 'config' }
+    ]
+  });
+
+  testStopWhere('spoor incorrect version', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.13' }]
+  });
 });
 
 /**
@@ -121,19 +240,48 @@ describe('adapt-contrib-spoor - to v2.1.1', async () => {
   let spoorConfig;
   const shouldRecordInteractionsPath = '_tracking._shouldRecordInteractions';
   whereFromPlugin('adapt-contrib-spoor - from <v2.1.1', { name: 'adapt-contrib-spoor', version: '<2.1.1' });
+
   whereContent('adapt-contrib-spoor - where missing _spoor._tracking._shouldRecordInteractions', async () => {
     spoorConfig = getSpoorConfig();
     if (!spoorConfig) return false;
     return !_.has(spoorConfig, shouldRecordInteractionsPath);
   });
+
   mutateContent('adapt-contrib-spoor - add _spoor._tracking._shouldRecordInteractions', async () => {
     _.set(spoorConfig, shouldRecordInteractionsPath, true);
     return true;
   });
+
   checkContent('adapt-contrib-spoor - check _spoor._tracking._shouldRecordInteractions added', async () => {
     const isValid = _.has(spoorConfig, shouldRecordInteractionsPath);
     if (!isValid) throw new Error(`_spoor.${shouldRecordInteractionsPath} not added`);
     return true;
   });
+
   updatePlugin('adapt-contrib-spoor - update to v2.1.1', { name: 'adapt-contrib-spoor', version: '2.1.1', framework: '>=2.0.16' });
+
+  testSuccessWhere('config with empty spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.12' }],
+    content: [
+      { _type: 'config', _spoor: {} }
+    ]
+  });
+
+  testSuccessWhere('config with spoor._tracking', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.12' }],
+    content: [
+      { _type: 'config', _spoor: { _tracking: {} } }
+    ]
+  });
+
+  testStopWhere('no spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.12' }],
+    content: [
+      { _type: 'config' }
+    ]
+  });
+
+  testStopWhere('spoor incorrect version', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '2.0.13' }]
+  });
 });
--- migrations/v4.js
+++ migrations/v4.js
@@ -5,7 +5,9 @@ import {
   mutateContent,
   checkContent,
   updatePlugin,
-  getConfig
+  getConfig,
+  testStopWhere,
+  testSuccessWhere
 } from 'adapt-migrations';
 import _ from 'lodash';
 
@@ -21,20 +23,56 @@ describe('adapt-contrib-spoor - to v4.1.1', async () => {
   const oldShouldSubmitScorePath = '_tracking._shouldSubmitScore';
   const shouldSubmitScorePath = '_completionCriteria._shouldSubmitScore';
   whereFromPlugin('adapt-contrib-spoor - from <v4.1.1', { name: 'adapt-contrib-spoor', version: '<4.1.1' });
+
   whereContent('adapt-contrib-spoor - where _spoor', async () => {
     config = getConfig();
     spoorConfig = getSpoorConfig();
     return spoorConfig;
   });
+
   mutateContent('adapt-contrib-spoor - replace _spoor._tracking._shouldSubmitScore with _completionCriteria._shouldSubmitScore', async () => {
     if (!_.has(config, shouldSubmitScorePath)) _.set(config, shouldSubmitScorePath, _.get(spoorConfig, oldShouldSubmitScorePath, false));
     _.unset(spoorConfig, oldShouldSubmitScorePath);
     return true;
   });
+
   checkContent('adapt-contrib-spoor - check _spoor._tracking._shouldSubmitScore replaced', async () => {
     const isValid = !_.has(spoorConfig, oldShouldSubmitScorePath) && _.has(config, shouldSubmitScorePath);
     if (!isValid) throw new Error(`_spoor.${oldShouldSubmitScorePath} not replaced`);
     return true;
   });
+
   updatePlugin('adapt-contrib-spoor - update to v4.1.1', { name: 'adapt-contrib-spoor', version: '4.1.1', framework: '>=5.17.8' });
+
+  testSuccessWhere('config with empty spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '4.1.0' }],
+    content: [
+      { _type: 'config', _spoor: {} }
+    ]
+  });
+
+  testSuccessWhere('config with default spoor._tracking', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '4.1.0' }],
+    content: [
+      { _type: 'config', _spoor: { _tracking: { _shouldSubmitScore: true } } }
+    ]
+  });
+
+  testSuccessWhere('config with empty spoor._tracking', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '4.1.0' }],
+    content: [
+      { _type: 'config', _spoor: { _tracking: {} } }
+    ]
+  });
+
+  testStopWhere('no spoor', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '4.1.0' }],
+    content: [
+      { _type: 'config' }
+    ]
+  });
+
+  testStopWhere('spoor incorrect version', {
+    fromPlugins: [{ name: 'adapt-contrib-spoor', version: '4.1.1' }]
+  });
 });
